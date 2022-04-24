a073eaf7cf14e4a082a29b9383122add6d4299f4
ease <easeava@gmail.com>
2022-04-24 11:20:02 +0800

    feat: add category

--- src/pages/main/category/index.tsx
+++ src/pages/main/category/index.tsx
@@ -1,17 +1,41 @@
 import { View } from '@tarojs/components';
-import { defineComponent } from 'vue';
+import { defineComponent, reactive } from 'vue';
+import { Tabs, TabPane } from '@nutui/nutui-taro';
+
+import './style.scss';
 
 export default defineComponent({
   name: 'Category',
 
   setup () {
-    //
+    const model = reactive({
+      currentTab: 0
+    });
+
+    return {
+      model,
+    };
   },
 
   render () {
+    const {
+      model,
+    } = this;
+
     return (
-      <View class='page'>
-        category
+      <View class='page page-category'>
+        <View class='page-header'>
+          header
+        </View>
+        <View class='page-tabs'>
+          <Tabs style={{height: '100%'}} v-model={model.currentTab} titleScroll direction='vertical'>
+            {[...Array(20)].map((_, item) => {
+              return <TabPane title={`Tab ${item}`}>
+                Tab {{item}}
+              </TabPane>;
+            })}
+          </Tabs>
+        </View>
       </View>
     );
   }
